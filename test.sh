25d85524689028489c935dd9c1a5628ff725bdc5
cielitive <15063942+cielitive@users.noreply.github.com>
2022-09-13 12:58:42 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,28 @@
-HOSTNAME=$(hostname -s)
-if [[ ${HOSTNAME} = "KCMNAN0001" ]]; then
-  ENV=development
-elif [[ ${HOSTNAME} = "HCMNAN0001" ]]; then
-  ENV=production
-elif [[ ${HOSTNAME} = "DCMNAN0001" ]]; then
-  ENV=dr
-fi
+#!/usr/bin/bash
+
+input=$1
+output="output.tmp"
+cp -p "$input" input.bk
+
+echo "tomcat:" >"$output"
+echo "  instance:" >>"$output"
+
+IFS=$'\n'
+for line in $(cat "$input" | sed '1,2d')
+do
+  echo "$line" >>"$output"
+
+  name=$(echo $line | awk -F', |: ' '{print $2}')
+  echo $name
+  num=$(echo $line | awk -F', |: ' '{print $4}')
+  echo $num
+
+  if [[ "$name" == "service" || "$name" == "test" ]] && [[ "$num" == 1 ]]; then
+    for i in $(seq 1 5)
+    do
+      echo "    - { num: $(( $num + $i )) }" >>"$output"
+    done
+  fi
+done
+
+mv "$output" "$input"
